ac88d3c9417032f36d70d0e8f33a69a391758a6b
Manuel Hartmann <manuel.hartmann@mailbox.org>
2016-11-26 10:04:31 +0100

    add ivdata project (node, cmd)

---
+++ ivdata/index.js
@@ -0,0 +1,70 @@
+#!/usr/bin/env node
+'use strict';
+/**
+ * Require dependencies
+ *
+ */
+const program = require('commander'),
+    chalk = require("chalk"),
+    exec = require('child_process').exec,
+    pkg = require('./package.json');
+
+/**
+ * list function definition
+ *
+ */
+let list = (directory,options)  => {
+    const cmd = 'ls';
+    let params = [];
+    
+    if (options.all) params.push("a");
+    if (options.long) params.push("l");
+    let parameterizedCommand = params.length 
+                                ? cmd + ' -' + params.join('') 
+                                : cmd ;
+    if (directory) parameterizedCommand += ' ' + directory ;
+    
+    let output = (error, stdout, stderr) => {
+        if (error) console.log(chalk.red.bold.underline("exec error:") + error);
+        if (stdout) console.log(chalk.green.bold.underline("Result:") + stdout);
+        if (stderr) console.log(chalk.red("Error: ") + stderr);
+    };
+    
+    exec(parameterizedCommand,output);
+    
+};
+
+
+program
+    .version(pkg.version)
+    .command('list [directory]')
+    .option('-a, --all', 'List all')
+    .option('-l, --long','Long list format')
+    .action(list);
+
+program.parse(process.argv);
+
+// if program was called with no arguments, show help.
+if (program.args.length === 0) program.help();
+
+
+
+// #!/usr/bin/env node
+// 'use strict';
+
+// const program = require('commander'),
+// 	pkg = require('./package.json');
+
+// console.log('Hello, world!');
+
+// program
+//     .version(pkg.version)
+//     .command('list [directory]')
+//     .option('-a, --all', 'List all')
+//     .option('-l, --long','Long list format')
+//     .action(list);
+
+// program.parse(process.argv);
+
+// // if program was called with no arguments, show help.
+// if (program.args.length === 0) program.help();
