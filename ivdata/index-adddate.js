2b58ba0308483eeebbf3762871d7ad147cdf757b
Manuel Hartmann <hartmannm@cip.ifi.lmu.de>
2016-11-27 20:42:44 +0100

    improved adddate command, it now also converts numeric values

--- ivdata/index-adddate.js
+++ ivdata/index-adddate.js
@@ -34,6 +34,36 @@ process.stdin.on('end', function() {
     // console.log(placeCount);
 
     for (var i = 0; i < placeCount; i++) {
+        if (json[i].spot) {
+            json[i].uid = Number(json[i].uid);
+            json[i].lat = Number(json[i].lat);
+            json[i].lng = Number(json[i].lng);
+            json[i].spot = Number(json[i].spot);
+            json[i].number = Number(json[i].number);
+            json[i].bike_racks = Number(json[i].bike_racks);
+            json[i].free_racks = Number(json[i].free_racks);
+
+            if (json[i].bike_numbers) {
+                var bikeNumbers = json[i].bike_numbers.split(',');
+                // console.log(bikeNumbers);
+                // console.log(bikeNumbers.length);
+                
+                json[i].bike_numbers = [];
+                for (var j = 0; j < bikeNumbers.length; j++) {
+                     // console.log(bikeNumbers[j]);
+                    json[i].bike_numbers[j] = Number(bikeNumbers[j]);
+                }
+            }
+        }
+        else {
+            json[i].uid = Number(json[i].uid);
+            json[i].lat = Number(json[i].lat);
+            json[i].lng = Number(json[i].lng);
+            json[i].bikes = Number(json[i].bikes);
+            json[i].bike = Number(json[i].bike);
+            json[i].bike_numbers = Number(json[i].bike_numbers);
+        }
+
         if(!json[i].date) {
             json[i].date = date;
             // console.log(json[i]);
