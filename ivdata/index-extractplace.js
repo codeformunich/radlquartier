7c41bc96137c8ab1712f222d4960d78549389c08
Manuel Hartmann <hartmannm@cip.ifi.lmu.de>
2016-11-27 19:45:20 +0100

    improved extractplace command, to handle inconsistent data

--- ivdata/index-extractplace.js
+++ ivdata/index-extractplace.js
@@ -16,18 +16,26 @@ process.stdin.on('end', function() {
         places;
 
     json = JSON.parse(input);
-    // console.log(json.markers.country.city.length);
+    // console.log(input);
+    // console.log(json);
 
     citys = json.markers.country.city;
-    cityCount = citys.length;
-    // console.log(citys[0].name);
-    // console.log(cityCount);
+    
+    if( citys.constructor === Array ) {
+        cityCount = citys.length;
+        // console.log(citys[0].name);
+        // console.log(cityCount);
 
-    for (var i = 0; i < cityCount; i++) {
-        if (citys[i].name === 'München') {
-            // console.log(json.markers.country.city[i]);
-            places = citys[i].place;
-        }
+        for (var i = 0; i < cityCount; i++) {
+            if (citys[i].name === 'München') {
+                // console.log(json.markers.country.city[i]);
+                places = citys[i].place;
+            }
+        }    
+    }
+    else {
+        // console.log(citys.name);
+        places = citys.place;
     }
 
     console.log(JSON.stringify(places, null, '\t'));
