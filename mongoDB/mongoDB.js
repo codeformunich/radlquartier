59f5038475357dc426508eda84c1784d0db319d9
Manuel Hartmann <hartmannm@cip.ifi.lmu.de>
2016-11-27 21:43:33 +0100

    add more queries to mongoDB.js

--- mongoDB/mongoDB.js
+++ mongoDB/mongoDB.js
@@ -2,9 +2,16 @@
 // conn = new Mongo();
 // db = conn.getDB("infovis");
 
+
 // cursor = db.features.find( { "properties.name": "Amalienburgstraße" } );
 
-cursor = db.features.find( { "properties.bike_numbers": "96486" } );
+// all features where bike_numbers is or contains 96570
+// cursor = db.features.find( { "properties.bike_numbers": 96570 }, { _id: 0 } );
+
+// all stations
+cursor = db.features.find( { "properties.spot": 1, "properties.date" : "2016-06-20T22:00:00.000Z" }, { _id: 0 } );
+
+
 
 printjson( cursor.toArray() );
 
