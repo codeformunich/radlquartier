7b95b6da1223774452088fd36b2c2259f0678726
Manuel Hartmann <hartmannm@cip.ifi.lmu.de>
2016-11-28 19:08:05 +0100

    improved querys

--- mongoDB/mongoDB.js
+++ mongoDB/mongoDB.js
@@ -6,16 +6,23 @@
 // cursor = db.features.find( { "properties.name": "Amalienburgstraße" } );
 
 // all features where bike_numbers is or contains 96570
-// cursor = db.features.find( { "properties.bike_numbers": 96570 }, { _id: 0 } );
+cursor = db.features.find( { 
+		"properties.date": { "$gt": new Date("2016-06-22T00:00:00") },
+		"properties.date": { "$lt": new Date("2016-06-23T00:00:00") },
+		"properties.bike_numbers": 96570 
+	}, { 
+         type: 1, geometry: 1, "properties.name": 1, _id: 0
+    } 
+);
 
 // all stations
-cursor = db.features.find( { 
-        "properties.date": { "$eq": new Date("2016-06-22T06:00:00") },
-        "properties.spot": 1
-    }, { 
-        type: 1, geometry: 1, "properties.name": 1, _id: 0
-    }
-);
+// cursor = db.features.find( { 
+//         "properties.date": { "$eq": new Date("2016-06-22T06:00:00") },
+//         "properties.spot": 1
+//     }, { 
+//         type: 1, geometry: 1, "properties.name": 1, _id: 0
+//     }
+// );
 
 printjson( cursor.toArray() );
 
