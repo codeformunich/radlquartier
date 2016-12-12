07b859c85cd40e8464072f983cabb232d3afe8be
Manuel Hartmann <hartmannm@cip.ifi.lmu.de>
2016-12-12 12:38:33 +0100

    improved querys

--- mongoDB/mongoDB.js
+++ mongoDB/mongoDB.js
@@ -10,24 +10,24 @@
 //          "properties.date": { "$gt": new Date("2016-06-22T00:00:00") },
 //          "properties.date": { "$lt": new Date("2016-06-23T00:00:00") },
 //          "properties.bike_numbers": 96570 
-//     }, { 
-//          type: 1, geometry: 1, "properties.name": 1, _id: 0
-//     } 
+//     }//, { 
+//     //     type: 1, geometry: 1, "properties.name": 1, _id: 0
+//     //} 
 // );
 // printjson( cursor.toArray() );
 
 
 // // all features where bike_numbers is or contains 96570
-cursor = db.features.find( { 
-         "properties.date": { "$gt": new Date("2016-08-01T00:00:00") },
-         "properties.date": { "$lt": new Date("2016-08-31T00:00:00") },
-         "properties.bike_numbers": 96111,
-         "properties.spot": 1
-    }, { 
-         type: 1, geometry: 1, "properties.name": 1, "properties.bike_numbers": 1, _id: 0
-    } 
-);
-printjson( cursor.toArray() );
+// cursor = db.features.find( { 
+//          "properties.date": { "$gt": new Date("2016-08-01T00:00:00") },
+//          "properties.date": { "$lt": new Date("2016-08-31T00:00:00") },
+//          "properties.bike_numbers": 96111,
+//          "properties.spot": 1
+//     }, { 
+//          type: 1, geometry: 1, "properties.name": 1, "properties.bike_numbers": 1, _id: 0
+//     } 
+// );
+// printjson( cursor.toArray() );
 
 
 // // all stations
@@ -43,13 +43,37 @@ printjson( cursor.toArray() );
 
 
 // // all distinct names
-// printjson( db.features.distinct( "properties.name" ) );
+ // printjson( db.features.distinct( "properties.name" ) );
 
 
 
-// // all stations
-// printjson( db.features.distinct( "properties.name", { "properties.spot": 1 } ) );
+// all stations
+// printjson( db.features.distinct( "properties.name", { "properties.spot": 1 } )); //.sort( { user_id: -1 } ) );
 
+
+// printjson( db.features.aggregate([
+//     { $project: { properties.name: "$_id", _id: 0}},
+//     { $sort: { properties.name: 1 } }
+// ]));
+
+// printjson(
+// 	db.features.aggregate([
+// 	    {$unwind: "$properties" },
+// 	    { $project : { "$properties.name":{$toUpper:"$_id"} , _id:0 } },
+// 	    { $sort: { "$properties.name": 1 } }
+// 	 ])
+// );
+
+printjson(
+	db.features.aggregate(
+		[ 
+		    { "$project" : { "name" : "$properties.name" , "_id" : "$_id"}},
+		    { "$unwind" : "$properties"},
+		    { "$sort" : { "$properties.name": 1 } },
+		    { "$project" : { "_id" : 0 , "texts" : 1}} 
+		]
+	)
+);
 
 
 // // // all bikes by name
