594357342523e9f3e6bffed2110fc70a513e1dfd
Manuel Hartmann <manuel.hartmann@mailbox.org>
2017-05-11 12:13:57 +0200

    add query to find all halts in Laim

---
+++ sh/mongo.sh
@@ -0,0 +1,15 @@
+#! /bin/zsh
+
+mongo bikeproject ridesPerDay.js > ridesPerDay.json
+
+mongo bikeproject ridesPerMonth.js > ridesPerMonth.json
+
+mongo bikeproject ridesPerWeek.js > ridesPerWeek.json
+
+mongo bikeproject ridesPerDay.js > ridesPerDay.json
+
+mongo bikeproject ridesPerDay.js > ridesPerHour.json
+
+mongo bikeproject ridesPerHour.js > ridesPerHour.json
+
+mongo bikeproject haltsInLaim.js > haltsInLaim.json
