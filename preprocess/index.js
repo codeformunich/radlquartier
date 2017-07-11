6e0f96a13e9aa87283dddd55fcff405a40bbbdd7
Manuel Hartmann <manuel.hartmann@mailbox.org>
2017-07-11 15:15:34 +0200

    renamed preprocess command parameters

--- preprocess/index.js
+++ preprocess/index.js
@@ -11,14 +11,13 @@ const program = require('commander'),
 
 program
     .version(pkg.version)
-    .command('coorswitch','switch coordinats in a geojson file')
+    .command('coorswitch', 'switch coordinats in a geojson file')
     .command('geojson', 'transforms place elments in a geojson file')
-    .command('linestring <bike>','cretaes a geojson linestring')
+    .command('linestring <bike>','cretaes a geojson linestring: node index.js linestring 96300 > bike96300.json')
     .command('chorddata', 'loads places data from the DB and aggregates them to data for the chord view')
-    .command('quartier','number of halts per destrict')
-    .command('qhw < ../geojson/munich.geojson','mean number of halts per destrict per week')
-    .command('qhm','mean number of halts per destrict per month')
-    .command('haltcoor','coordinates of halts per destrict')
+    .command('quartier', 'number of halts per district: node index.js quartier < ../geojson/munich.geojson > district.json')
+    .command('qhs', 'halts statistic per district: node index.js preprocess qh < ../geojson/munich.geojson')
+    .command('qhc', 'coordinates of halts per destrict: node index.js haltcoor < ../geojson/munich.geojson > haltsperdistrict.json')
     .command('stations', 'extract stations as geojson from a "networkstate"-file: preprocess stations < networkstate.json')
     .parse(process.argv);
 
