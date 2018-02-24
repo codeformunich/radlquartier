6f6daf791e43782564d3f6b106b3778890278d19
Manuel Hartmann <manuel.hartmann@mailbox.org>
2018-02-24 23:01:32 +0100

    clean-up package.json files, mongodb queries

--- datatool/queries/stationsUidAndName.js
+++
@@ -1,8 +0,0 @@
-
-// all stations by name
-cursor = db.nextbike.aggregate( [
-    { $match: { spot: 1 } },
-    { $group: { _id: { uid: "$uid" }, name: { $addToSet: "$name" } } },
-    { $sort : { name : 1 } }
-] );
-printjson(cursor.toArray());
