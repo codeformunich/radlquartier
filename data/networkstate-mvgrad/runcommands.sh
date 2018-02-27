156d5878afaed2f018a6d577abadceff540a9419
Manuel Hartmann <manuel.hartmann@mailbox.org>
2018-02-28 00:50:02 +0100

    renamed folder

--- data/networkstate-mvgrad/runcommands.sh
+++ data/networkstate-mvgrad/runcommands.sh
@@ -1,7 +1,14 @@
 #! /bin/zsh
 
+echo "creating halts from networkstate-mvgrad files"
 node ./../../datatool/mvgstate/mvgstate.js halt ./input
+
+echo "creating stations from networkstate-mvgrad files"
 node ./../../datatool/mvgstate/mvgstate.js station ./input
+
+echo "creating geojson points from halts"
+node ./../../datatool/preprocess/preprocess.js stationtopoint < ./output/mvgStation.json
+
 
 # node ./../../datatool/mvgstate/mvgstate.js station ./input/2017/03
 # node ./../../datatool/mvgstate/mvgstate.js halt ./input/2017/04
