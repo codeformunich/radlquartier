be799d93052a4d6ade70efb302fc7a531cfdc7a7
Manuel Hartmann <manuel.hartmann@mailbox.org>
2018-04-12 19:19:09 +0200

    fixed bug in chart.js, check for selectedData undefined improved indention

--- docs/js/app.js
+++ docs/js/app.js
@@ -1,103 +1,119 @@
 var App = (function(window, document, d3) {
-    var selctedDistrict = 0,
-        disrticts = null;
+  var selctedDistrict = 0,
+    disrticts = null;
 
-    d3.json('data/districts.json', function(error, districtsData) {
-        districts = districtsData;
+  d3.json('data/districts.json', function(error, districtsData) {
+    districts = districtsData;
+  });
+
+  function selectNewDistrict(id) {
+    console.log({ type: 'selectNewDistrict', id: id, context: this });
+
+    if (id !== 0) {
+      $('#detailCollapse').collapse('show');
+
+      selctedDistrict = id;
+
+      Text.loadDistrict(id);
+      Chart.loadDistrict(id);
+      Hexmap.loadDistrict(id);
+
+      setNavBar();
+
+      var target = $('#detailCollapse');
+      if (target.length) {
+        event.preventDefault();
+        $('html, body').animate(
+          {
+            scrollTop: target.offset().top
+          },
+          1050
+        ); // 500 (0.5sek) legt Geschwindkeit fest
+      }
+    } else {
+      $('#detailCollapse').collapse('hide');
+
+      selctedDistrict = 0;
+    }
+  }
+
+  function setNavBar() {
+    var index,
+      previousButton,
+      nextButton,
+      previousDistrict = 0,
+      nextDistrict = 1;
+
+    if (
+      selctedDistrict === 0 ||
+      districts === null ||
+      districts === undefined
+    ) {
+      return;
+    }
+
+    index = districts.findIndex(function(district) {
+      return district.id === selctedDistrict;
     });
-
-    function selectNewDistrict(id) {
-        console.log({ type: 'selectNewDistrict', id: id, context: this });
-
-        if (id !== 0) {
-            $('#detailCollapse').collapse('show');
-
-            selctedDistrict = id;
-
-            Text.loadDistrict(id);
-            Chart.loadDistrict(id);
-            Hexmap.loadDistrict(id);
-
-            setNavBar();
-
-            var target = $('#detailCollapse');
-            if (target.length) {
-                event.preventDefault();
-                $('html, body').animate({
-                    scrollTop: target.offset().top
-                }, 1050); // 500 (0.5sek) legt Geschwindkeit fest
-            }
-        } else {
-            $('#detailCollapse').collapse('hide');
-
-            selctedDistrict = 0;
-        }
+    if (index === 0) {
+      previousDistrict = 0;
+      nextDistrict = 1;
+    } else if (index === 24) {
+      previousDistrict = 23;
+      nextDistrict = 24;
+    } else {
+      previousDistrict = index - 1;
+      nextDistrict = index + 1;
     }
 
-    function setNavBar() {
-        var index,
-            previousButton,
-            nextButton,
-            previousDistrict = 0,
-            nextDistrict = 1;
+    previousButton = $('#previousbutton');
+    nextButton = $('#nextbutton');
 
-        if (selctedDistrict === 0 || districts === null || districts === undefined) {
-            return;
-        }
+    previousButton.val(districts[previousDistrict].id);
+    previousButton.html(
+      '<span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span> ' +
+        districts[previousDistrict].name
+    );
 
-        index = districts.findIndex(function(district) {
-            return district.id === selctedDistrict;
-        });
-        if (index === 0) {
-            previousDistrict = 0;
-            nextDistrict = 1;
-        } else if (index === 24) {
-            previousDistrict = 23;
-            nextDistrict = 24;
-        } else {
-            previousDistrict = index - 1;
-            nextDistrict = index + 1;
-        }
+    nextButton.val(districts[nextDistrict].id);
+    nextButton.html(
+      districts[nextDistrict].name +
+        ' <span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span>'
+    );
+  }
 
-        previousButton = $('#previousbutton');
-        nextButton = $('#nextbutton');
+  function clickedNextDistrict(e) {
+    console.log({ type: 'clickedPreviousDistrict', event: e, context: this });
 
-        previousButton.val(districts[previousDistrict].id);
-        previousButton.html('<span class="glyphicon glyphicon-arrow-left" aria-hidden="true"></span> ' + districts[previousDistrict].name);
+    var id = +e.currentTarget.value;
 
-        nextButton.val(districts[nextDistrict].id);
-        nextButton.html(districts[nextDistrict].name + ' <span class="glyphicon glyphicon-arrow-right" aria-hidden="true"></span>');
+    selectNewDistrict(id);
+  }
+
+  function clickedUp(e) {
+    console.log({ type: 'clickedUp', event: e, context: this });
+    var target = $('#top');
+    if (target.length) {
+      event.preventDefault();
+      $('html, body').animate(
+        {
+          scrollTop: target.offset().top
+        },
+        1050
+      ); // 500 (0.5sek) legt Geschwindkeit fest
     }
+  }
 
-    function clickedNextDistrict(e) {
-        console.log({ type: 'clickedPreviousDistrict', event: e, context: this });
+  $(document).ready(function() {
+    window.addEventListener('resize', Chart.render);
+    window.addEventListener('resize', Mucmap.render);
 
-        var id = +e.currentTarget.value;
+    $('#previousbutton').click(clickedNextDistrict);
+    $('#nextbutton').click(clickedNextDistrict);
+    $('#upbutton').click(clickedUp);
+  });
 
-        selectNewDistrict(id);
-    }
-
-    function clickedUp(e) {
-        console.log({ type: 'clickedUp', event: e, context: this });
-        var target = $('#top');
-        if (target.length) {
-            event.preventDefault();
-            $('html, body').animate({
-                scrollTop: target.offset().top
-            }, 1050); // 500 (0.5sek) legt Geschwindkeit fest
-        }
-    }
-
-    $(document).ready(function() {
-        window.addEventListener('resize', Chart.render);
-        window.addEventListener('resize', Mucmap.render);
-
-        $("#previousbutton").click(clickedNextDistrict);
-        $("#nextbutton").click(clickedNextDistrict);
-        $("#upbutton").click(clickedUp);
-    });
-
-    return {
-        selectNewDistrict: selectNewDistrict
-    };
+  return {
+    selectNewDistrict: selectNewDistrict
+  };
 })(window, document, d3);
--- website/js/chart.js
+++ website/js/chart.js
@@ -1,248 +1,257 @@
 var Chart = (function(d3, window, document, undefined) {
-    //https://bl.ocks.org/mbostock/3885304
+  //https://bl.ocks.org/mbostock/3885304
 
-    var month = [
-        'Januar',
-        'Februar',
-        'März',
-        'April',
-        'Mai',
-        'Juni',
-        'Juli',
-        'August',
-        'September',
-        'Oktober',
-        'November',
-        'Dezember'
-    ];
+  var month = [
+    'Januar',
+    'Februar',
+    'März',
+    'April',
+    'Mai',
+    'Juni',
+    'Juli',
+    'August',
+    'September',
+    'Oktober',
+    'November',
+    'Dezember'
+  ];
 
-    var week = [
-        'Montag',
-        'Dienstag',
-        'Mittwoch',
-        'Donnerstag',
-        'Freitag',
-        'Samstag',
-        'Sonntag'
-    ];
+  var week = [
+    'Montag',
+    'Dienstag',
+    'Mittwoch',
+    'Donnerstag',
+    'Freitag',
+    'Samstag',
+    'Sonntag'
+  ];
 
-    var year = [
-        'Viertel',
-        'Absolut'
-    ];
+  var year = ['Viertel', 'Absolut'];
 
-    var districtsData,
-        selectedData,
-        selectedDistrict = null,
-        selectedChart = 'week',
-        selectedMean = 0,
-        yDomainMax = 300;
+  var districtsData,
+    selectedData,
+    selectedDistrict = null,
+    selectedChart = 'week',
+    selectedMean = 0,
+    yDomainMax = 300;
 
-    var parentWidth,
-        parentHeight;
+  var parentWidth, parentHeight;
 
-    // sets width and height for the bar chart
+  // sets width and height for the bar chart
+  updateDimensions();
+
+  d3.select('input[value="week"]').property('checked', true);
+
+  d3.selectAll('input').on('change', selectDataset);
+
+  function selectDataset() {
+    selectedChart = this.value;
+    selectionChanged();
+  }
+
+  var div = d3.select('.toolTip');
+
+  var svg = d3.select('#barchart').select('svg'),
+    margin = { top: 20, right: 20, bottom: 30, left: 40 },
+    // width = +svg.attr("width") - margin.left - margin.right,
+    // height = +svg.attr("height") - margin.top - margin.bottom;
+    width = parentWidth - margin.left - margin.right,
+    height = parentHeight - margin.top - margin.bottom;
+
+  var x, //= d3.scaleBand().rangeRound([0, width]).padding(0.1),
+    y; //= d3.scaleLinear().rangeRound([height, 0]);
+
+  var g = svg
+    .append('g')
+    .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+  d3.json('data/district_halts.json', function(error, json) {
+    if (error) throw error;
+
+    districtsData = json;
+    selectionChanged();
+  });
+
+  function render() {
+    if (!selectedData) {
+      return;
+    }
+
+    //get dimensions based on window size
     updateDimensions();
 
-    d3.select("input[value=\"week\"]").property("checked", true);
+    // //line function for averageLine
+    // var averageline = d3.line()
+    //     .x(function(d) { return x(d.name); })
+    //     .y(function(d) { return y(selectedMean); });
+    //     // .y(function(d) { return y(d.value); });
 
-    d3.selectAll("input").on("change", selectDataset);
+    var t = d3
+      .transition()
+      .duration(1000)
+      .ease(d3.easeLinear);
 
-    function selectDataset() {
-        selectedChart = this.value;
-        selectionChanged();
+    svg.attr('width', parentWidth).attr('height', parentHeight);
+
+    width = parentWidth - margin.left - margin.right;
+    height = parentHeight - margin.top - margin.bottom;
+
+    x = d3
+      .scaleBand()
+      .rangeRound([0, width])
+      .padding(0.1);
+    y = d3.scaleLinear().rangeRound([height, 0]);
+
+    x.domain(
+      selectedData.map(function(d) {
+        return d.name;
+      })
+    );
+    //  d3.max(selectedData, function(d) {
+    //     return d.value; })]);
+    y.domain([0, yDomainMax]);
+
+    svg.select('.axis.axis--y').remove();
+    svg.select('.axis.axis--x').remove();
+
+    g
+      .append('g')
+      .attr('class', 'axis axis--x')
+      .attr('transform', 'translate(0,' + height + ')')
+      .call(d3.axisBottom(x));
+
+    g
+      .append('g')
+      .attr('class', 'axis axis--y')
+      .call(d3.axisLeft(y).ticks(10, 's'))
+      .append('text')
+      .attr('transform', 'rotate(-90)')
+      .attr('y', 6)
+      .attr('dy', '0.71em')
+      .attr('text-anchor', 'end')
+      .text('Test');
+
+    var bar = g
+      .selectAll('.bar')
+      .remove()
+      .exit()
+      .data(selectedData);
+
+    // bar.exit().remove();
+
+    bar
+      .enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('x', function(d) {
+        return x(d.name);
+      })
+      .attr('y', function(d) {
+        return y(d.value);
+      })
+      // .attr("y", function(d) { return y(0); })
+      .attr('width', x.bandwidth())
+      .attr('height', function(d) {
+        return height - y(d.value);
+      })
+      .on('mouseout', mouseout)
+      .on('mousemove', mousemove);
+    // .attr("height", function(d) { return height - y(0); })
+    // .merge(bar);
+
+    // removed data:
+    // bar.exit().remove();
+
+    // updated data:
+    // bar
+    //   .transition(t)
+    //   .attr("x", function(d) { return x(d.name); })
+    //   .attr("y", function(d) { return y(d.value); })
+    //   .attr("width", x.bandwidth())
+    //   .attr("height", function(d) { return height - y(d.value); });
+
+    // // add lien to chart
+    // svg.select(".line").remove();
+
+    // // var line = g.selectAll(".line")
+    //     g.append("path")        // Add the valueline path.
+    //     .attr("class", "line")
+    //     .attr("d", averageline(selectedData));
+  }
+
+  function mouseout(d) {
+    div.style('display', 'none');
+  }
+
+  function mousemove(d) {
+    div.style('left', d3.event.pageX + 10 + 'px');
+    div.style('top', d3.event.pageY - 25 + 'px');
+    div.style('display', 'inline-block');
+    div.html(Math.round(d.value) + ' Fahrten');
+  }
+
+  function updateDimensions() {
+    parentWidth = parseInt(d3.select('#barchart').style('width'));
+    parentHeight = 0.4 * parentWidth; //aspect ratio is 0.78
+  }
+
+  function selectionChanged() {
+    if (selectedDistrict === null) {
+      return;
     }
 
-    var div = d3.select(".toolTip");
+    if (selectedChart == 'week') {
+      yDomainMax = 250;
+      selectedMean = d3.mean(selectedDistrict.meanWeekDays);
+      selectedData = weekData(selectedDistrict);
+      render();
+    } else if (selectedChart == 'month') {
+      yDomainMax = 10000;
+      selectedMean = selectedDistrict.meanMonth;
+      selectedData = monthData(selectedDistrict);
+      render();
+    } else if (selectedChart == 'year') {
+      yDomainMax = 500000;
+      selectedData = yearData(selectedDistrict);
+      render();
+    }
+  }
 
-    var svg = d3.select("#barchart").select('svg'),
-        margin = { top: 20, right: 20, bottom: 30, left: 40 },
-        // width = +svg.attr("width") - margin.left - margin.right,
-        // height = +svg.attr("height") - margin.top - margin.bottom;
-        width = parentWidth - margin.left - margin.right,
-        height = parentHeight - margin.top - margin.bottom;
+  function weekData(district) {
+    var data = [];
+    for (var i = 0; i < week.length; i++) {
+      data.push({ name: week[i], value: district.meanWeekDays[i] });
+    }
+    return data;
+  }
 
-    var x, //= d3.scaleBand().rangeRound([0, width]).padding(0.1),
-        y; //= d3.scaleLinear().rangeRound([height, 0]);
+  function monthData(district) {
+    var data = [];
+    for (var i = 0; i < month.length; i++) {
+      data.push({ name: month[i], value: district.meanYearMonth[i] });
+    }
+    return data;
+  }
 
-    var g = svg.append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  function yearData(district) {
+    var data = [];
 
+    data.push({ name: year[0], value: district.totalCount });
+    data.push({ name: year[1], value: districtsData.totalCount });
 
-    d3.json('data/district_halts.json', function(error, json) {
-        if (error) throw error;
+    return data;
+  }
 
-        districtsData = json;
-        selectionChanged();
+  function loadDistrict(id) {
+    selectedDistrict = districtsData.districts.find(function(district) {
+      return district.id === id;
     });
 
-    function render() {
-        //get dimensions based on window size
-        updateDimensions();
+    selectionChanged();
+  }
 
-        // //line function for averageLine
-        // var averageline = d3.line()
-        //     .x(function(d) { return x(d.name); })
-        //     .y(function(d) { return y(selectedMean); });
-        //     // .y(function(d) { return y(d.value); });
-
-
-        var t = d3.transition()
-            .duration(1000)
-            .ease(d3.easeLinear);
-
-        svg
-          .attr('width', parentWidth)
-          .attr('height', parentHeight);
-
-        width = parentWidth - margin.left - margin.right;
-        height = parentHeight - margin.top - margin.bottom;
-
-        x = d3.scaleBand().rangeRound([0, width]).padding(0.1);
-        y = d3.scaleLinear().rangeRound([height, 0]);
-
-        x.domain(selectedData.map(function(d) {
-            return d.name; }));
-        //  d3.max(selectedData, function(d) {
-        //     return d.value; })]);
-        y.domain([0, yDomainMax]);
-
-        svg.select(".axis.axis--y").remove();
-        svg.select(".axis.axis--x").remove();
-
-        g.append("g")
-            .attr("class", "axis axis--x")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x));
-
-        g.append("g")
-            .attr("class", "axis axis--y")
-            .call(d3.axisLeft(y).ticks(10, "s"))
-            .append("text")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", "0.71em")
-            .attr("text-anchor", "end")
-            .text("Test");
-
-        var bar = g.selectAll(".bar")
-            .remove()
-            .exit()
-            .data(selectedData);
-
-        // bar.exit().remove();
-
-        bar.enter().append("rect")
-            .attr("class", "bar")
-            .attr("x", function(d) {
-                return x(d.name); })
-            .attr("y", function(d) {
-                return y(d.value); })
-            // .attr("y", function(d) { return y(0); })
-            .attr("width", x.bandwidth())
-            .attr("height", function(d) { return height - y(d.value); })
-        .on('mouseout', mouseout)
-        .on('mousemove', mousemove);
-        // .attr("height", function(d) { return height - y(0); })
-        // .merge(bar);
-
-        // removed data:
-        // bar.exit().remove();
-
-        // updated data:
-        // bar
-        //   .transition(t)
-        //   .attr("x", function(d) { return x(d.name); })
-        //   .attr("y", function(d) { return y(d.value); })
-        //   .attr("width", x.bandwidth())
-        //   .attr("height", function(d) { return height - y(d.value); });
-
-
-        // // add lien to chart
-        // svg.select(".line").remove();
-
-        // // var line = g.selectAll(".line")
-        //     g.append("path")        // Add the valueline path.
-        //     .attr("class", "line")
-        //     .attr("d", averageline(selectedData));
-    }
-
-    function mouseout(d) {
-        div.style("display", "none");
-    }
-
-    function mousemove(d) {
-        div.style("left", d3.event.pageX + 10 + "px");
-        div.style("top", d3.event.pageY - 25 + "px");
-        div.style("display", "inline-block");
-        div.html((Math.round(d.value)) + " Fahrten");
-    }
-
-
-    function updateDimensions() {
-        parentWidth = parseInt(d3.select('#barchart').style('width'));
-        parentHeight = 0.4 * parentWidth; //aspect ratio is 0.78
-    }
-
-    function selectionChanged() {
-        if (selectedDistrict === null) {
-            return;
-        }
-
-        if (selectedChart == "week") {
-            yDomainMax = 250;
-            selectedMean  = d3.mean(selectedDistrict.meanWeekDays);
-            selectedData = weekData(selectedDistrict);
-            render();
-        } else if (selectedChart == "month") {
-            yDomainMax = 10000;
-            selectedMean = selectedDistrict.meanMonth;
-            selectedData = monthData(selectedDistrict);
-            render();
-        } else if (selectedChart == "year") {
-            yDomainMax = 500000;
-            selectedData = yearData(selectedDistrict);
-            render();
-        }
-    }
-
-    function weekData(district) {
-        var data = [];
-        for (var i = 0; i < week.length; i++) {
-            data.push({ 'name': week[i], 'value': district.meanWeekDays[i] });
-        }
-        return data;
-    }
-
-    function monthData(district) {
-        var data = [];
-        for (var i = 0; i < month.length; i++) {
-            data.push({ 'name': month[i], 'value': district.meanYearMonth[i] });
-        }
-        return data;
-    }
-
-    function yearData(district) {
-        var data = [];
-
-        data.push({ 'name': year[0], 'value': district.totalCount });
-        data.push({ 'name': year[1], 'value': districtsData.totalCount });
-
-        return data;
-    }
-
-    function loadDistrict(id) {
-        selectedDistrict = districtsData.districts.find( function(district) {
-            return district.id === id;
-        });
-
-        selectionChanged();
-    }
-
-    return {
-        render : render,
-        loadDistrict : loadDistrict
-    };
+  return {
+    render: render,
+    loadDistrict: loadDistrict
+  };
 })(d3, window, document);
-
-
