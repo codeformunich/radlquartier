8c63e6a06cd3f8cd24925963f54bda2e6a8568dc
Manuel Hartmann <manuel.hartmann@mailbox.org>
2018-04-19 17:20:30 +0200

    improved mvgstate tool to work faster (async I/O with promise)

--- datatool/mvgstate/mvgstate-halt.js
+++ datatool/mvgstate/mvgstate-halt.js
@@ -10,6 +10,7 @@ const program = require('commander');
 const path = require('path');
 const fs = require('fs');
 const uuidv4 = require('uuid/v4');
+const cliProgress = require('cli-progress');
 
 const outputFolder = 'output';
 const tempFileName = 'mvgStateTemp.json';
@@ -17,6 +18,9 @@ const dataFileName = 'mvgStateHalts.json';
 
 let haltData = null;
 let tempData = null;
+
+const bar2 = new cliProgress.Bar({}, cliProgress.Presets.shades_classic);
+let bar2Index = 0;
 
 program.parse(process.argv);
 const args = program.args;
@@ -28,61 +32,88 @@ const main = function() {
     inputFolder === undefined ||
     inputFolder.length === 0
   ) {
-    console.log('ERROR: main, inputFolder:', inputFolder);
+    console.error('main, inputFolder:', inputFolder);
     return;
   }
 
   const outputPathTemp = path.join(outputFolder, tempFileName);
   const outputPathData = path.join(outputFolder, dataFileName);
 
-  tempData = helper.loadJsonFile(outputPathTemp);
+  tempData = helper.loadJsonFileSync(outputPathTemp);
   if (tempData == null) {
-    console.log('INFO: main, create new tempData object');
+    console.log('main, create new tempData object');
     tempData = {};
   }
-  haltData = helper.loadJsonFile(outputPathData);
+  haltData = helper.loadJsonFileSync(outputPathData);
   if (haltData == null) {
-    console.log('INFO: main, create new haltData array');
+    console.log('main, create new haltData array');
     haltData = [];
   }
 
-/*  const filenames = helper.readDirectory(inputFolder);
-  filenames.forEach(function(filename) {
-    if (path.extname(filename) !== '.json') {
-      return;
-    }
+  // const filenames = helper.readDirectory(inputFolder);
+  // filenames.forEach(function(filename) {
+  //   if (path.extname(filename) !== '.json') {
+  //     return;
+  //   }
 
-    console.log('INFO: main, filename:', filename);
+  //   console.log('INFO: main, filename:', filename);
 
-    const filePath = path.join(inputFolder, filename);
-    const json = helper.loadJsonFile(filePath);
+  //   const filePath = path.join(inputFolder, filename);
+  //   const json = helper.loadJsonFile(filePath);
 
-    if (json == null) {
-      console.log('ERROR main, json:', json);
-      return;
-    }
+  //   if (json == null) {
+  //     console.log('ERROR main, json:', json);
+  //     return;
+  //   }
 
-    const bikes = json.addedBikes;
-    generateHalts(bikes);
-  });*/
-  processDirectory(inputFolder);
+  //   const bikes = json.addedBikes;
+  //   generateHalts(bikes);
+  // });
 
-  if (haltData.length === 0) {
-    console.log('ERROR main, haltData: empty');
-    return;
-  }
+  processDirectory(inputFolder)
+    .then(() => {
+      // stop the progress bar
+      bar2.stop();
 
-  helper.createDirectory(outputFolder);
-  helper.writeJsonFile(outputPathData, haltData);
-  helper.writeJsonFile(outputPathTemp, tempData);
+      if (haltData.length === 0) {
+        console.error('main, haltData: empty');
+        return;
+      }
 
-  console.log('INFO: main: Done!');
+      helper.createDirectorySync(outputFolder);
+      helper.writeJsonFileSync(outputPathData, haltData);
+      helper.writeJsonFileSync(outputPathTemp, tempData);
+
+      console.log('main, Done!');
+    })
+    .catch(error => {
+      console.error('main, error:', error.message);
+    });
 };
 
 const processDirectory = function(inputFolder) {
-  const filenames = helper.readDirectory(inputFolder);
-  filenames.forEach(function(filename) {
-    if (path.basename(filename) != '.' && path.basename(filename) != '..' && fs.lstatSync(path.resolve(inputFolder + '/' + filename)).isDirectory()) {
+  // create a new progress bar instance and use shades_classic theme
+  const bar1 = new cliProgress.Bar({}, cliProgress.Presets.shades_classic);
+
+  let promises = [];
+
+  const filenames = helper.readDirectorySync(inputFolder);
+
+  console.log('process directory');
+  // start the progress bar with a total value of 200 and start value of 0
+  bar1.start(filenames.length, 0);
+
+
+  filenames.forEach(function(filename, index) {
+    // update the current value in your application..
+    bar1.update(index + 1);
+    // console.log('processDirectory, filename:', filename);
+    
+    if (
+      path.basename(filename) != '.' &&
+      path.basename(filename) != '..' &&
+      fs.lstatSync(path.resolve(inputFolder + '/' + filename)).isDirectory()
+    ) {
       processDirectory(inputFolder + '/' + filename);
     }
 
@@ -90,26 +121,73 @@ const processDirectory = function(inputFolder) {
       return;
     }
 
-    console.log('INFO: main, filename:', filename);
-
     const filePath = path.join(inputFolder, filename);
-    const json = helper.loadJsonFile(filePath);
+    promises.push(
+      helper.readFile(filePath).then(
+        content => {
+          // update the current value in your application..
+          bar2Index += 1;
+          bar2.update(bar2Index);
+          // console.log('processDirectory, bar1:', bar1);
+          // console.log('processDirectory, barIndex:', barIndex);
 
-    if (json == null) {
-      console.log('ERROR main, json:', json);
-      return;
-    }
+          let json = null;
+          try {
+            json = JSON.parse(content);
+          } catch (error) {
+            console.error(
+              `processDirectory, filePath: ${filePath} error:, ${error}, filePath`
+            );
+            return;
+          }
 
-    const bikes = json.addedBikes;
-    if(bikes) {
-      generateHalts(bikes);
-    }
+          if (json == null) {
+            console.error('processDirectory, json:', json);
+            return;
+          }
+
+          const bikes = json.addedBikes;
+          if (bikes) {
+            generateHalts(bikes);
+          }
+        },
+        error => {
+          console.error('processDirectory, error:', error.message);
+          return;
+        }
+      )
+    );
+
+    // const json = helper.loadJsonFile(filePath);
+
+    // if (json == null) {
+    //   console.log('ERROR main, json:', json);
+    //   return;
+    // }
+
+    // const bikes = json.addedBikes;
+    // if (bikes) {
+    //   generateHalts(bikes);
+    // }
+
+    
+    
   });
+
+  // stop the progress bar
+  bar1.stop();
+
+  console.log('process bike data');
+  // start the progress bar with a total value of 200 and start value of 0
+  bar2.start(filenames.length, 0);
+  
+
+  return Promise.all(promises);
 };
 
 const generateHalts = function(rawBikes) {
-  if 
-  
+  // console.log('generateHalts, filename', filename);
+
   rawBikes.forEach(function(rawBike) {
     const lastExistingHalt = findLastExistingHalt(rawBike.bikeNumber);
 
@@ -125,23 +203,10 @@ const generateHalts = function(rawBikes) {
   });
 };
 
-const findLastExistingHalt = function(bikeNumber) {
-  if (tempData === null || tempData === undefined) {
-    console.log('ERROR: findLastHalt, tempData:', tempData);
-    return null;
-  }
-
-  if (tempData.hasOwnProperty(bikeNumber)) {
-    return tempData[bikeNumber];
-  }
-
-  return null;
-};
-
 const createHalt = function(bike) {
   const halt = newHalt(bike);
-  if (halt === null) {
-    console.log('ERROR: createHalt, halt: ', halt);
+  if (!halt) {
+    console.error('createHalt, halt: ', halt);
     return;
   }
 
@@ -150,9 +215,10 @@ const createHalt = function(bike) {
 };
 
 const updateHalt = function(bike, lastHalt) {
-  const index = haltData.findIndex(function(halt) {
-    return halt.id === lastHalt.id;
-  });
+  // const index = haltData.findIndex(function(halt) {
+  //   return halt.id === lastHalt.id;
+  // });
+  const index = findLastIndexForId(haltData, lastHalt);
 
   const date = new Date(bike.updated);
 
@@ -164,10 +230,11 @@ const updateHalt = function(bike, lastHalt) {
 };
 
 // helper function
+
 // creates new halt object
 const newHalt = function(rawBike) {
   if (rawBike === null || rawBike === undefined) {
-    console.log('ERROR: newHalt, bike:', rawBike);
+    console.error('newHalt, bike:', rawBike);
     return null;
   }
 
@@ -199,6 +266,37 @@ const newHalt = function(rawBike) {
   };
 
   return halt;
+};
+
+const findLastExistingHalt = function(bikeNumber) {
+  if (!tempData) {
+    console.error('findLastExistingHalt, tempData:', tempData);
+    return null;
+  }
+
+  if (tempData.hasOwnProperty(bikeNumber)) {
+    return tempData[bikeNumber];
+  }
+
+  return null;
+};
+
+const findLastIndexForId = function(data, lastHalt) {
+  // console.info('findLastIndexForId, data:', data);
+  // console.info('findLastIndexForId, lastHalt:', lastHalt);
+  // console.info('findLastIndexForId, data.length:', data.length);
+
+  for (let index = data.length - 1; index >= 0; index--) {
+    // console.info('findLastIndexForId, index:', index);
+
+    if (data[index].id == lastHalt.id) {
+      // console.info('findLastIndexForId, return:', index);
+      return index;
+    }
+  }
+
+  console.info('findLastIndexForId, return:', -1);
+  return -1;
 };
 
 main();
--- datatool/share/helper.js
+++ datatool/share/helper.js
@@ -6,13 +6,13 @@
 
 const fs = require('fs');
 
-exports.createDirectory = function (directory) {
+exports.createDirectorySync = function(directory) {
   if (!fs.existsSync(directory)) {
     fs.mkdirSync(directory);
   }
 };
 
-exports.readDirectory = function (directory) {
+exports.readDirectorySync = function(directory) {
   try {
     return fs.readdirSync(directory);
   } catch (error) {
@@ -21,7 +21,7 @@ exports.readDirectory = function (directory) {
   }
 };
 
-exports.loadJsonFile = function (filePath) {
+exports.loadJsonFileSync = function(filePath) {
   if (!fs.existsSync(filePath)) {
     return null;
   }
@@ -34,7 +34,7 @@ exports.loadJsonFile = function (filePath) {
   }
 };
 
-exports.writeJsonFile = function (filePath, data) {
+exports.writeJsonFileSync = function (filePath, data) {
   try {
     fs.writeFileSync(filePath, JSON.stringify(data, null, '\t'));
   } catch (error) {
@@ -42,3 +42,67 @@ exports.writeJsonFile = function (filePath, data) {
     throw error;
   }
 };
+
+
+
+exports.exists = function(filename) {
+  return new Promise(function(resolve, reject) {
+    // trigger the asynchronous operation
+    fs.exists(filename, function(err, contents) {
+      // check for errors
+      if (err) {
+        reject(err);
+        return;
+      } // the read succeeded
+
+      resolve(contents);
+    });
+  });
+};
+
+exports.readFile = function(filename) {
+  return new Promise(function(resolve, reject) {
+    // trigger the asynchronous operation
+    fs.readFile(filename, { encoding: 'utf8' }, function(err, contents) {
+      // check for errors
+      if (err) {
+        reject(err);
+        return;
+      } // the read succeeded
+
+      resolve(contents);
+    });
+  });
+};
+
+exports.writeFile = function(filename, data) {
+  return new Promise(function(resolve, reject) {
+    // trigger the asynchronous operation
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err, contents) {
+      // check for errors
+      if (err) {
+        reject(err);
+        return;
+      } // the write succeeded
+
+      resolve(contents);
+    });
+  });
+};
+
+
+
+
+// let promise = readFile('example.txt');
+
+// // listen for both fulfillment and rejection
+// promise.then(
+//   function(contents) {
+//     // fulfillment
+//     console.log(contents);
+//   },
+//   function(err) {
+//     // rejection
+//     console.error(err.message);
+//   }
+// );
